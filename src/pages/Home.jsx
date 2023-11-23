15fe2e4f052050834c20a1458bd314c8ca4dc62c
Saif Aljashamy <supersaifaljashamy@gmail.com>
2023-11-22 16:20:10 -0800

    Define React context with Valtio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,14 @@
-import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
+
+import{
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation
+} from '../config/motion'
 
 const Home = () => {
   return (
